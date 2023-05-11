38f3a05d04ba7c82ca5b956f328ff84298cdd489
Matthew Casperson <matthewcasperson@gmail.com>
2023-05-11 16:41:13 +1000

    Exclude by regex

--- management_instance/runbooks/shared_scripts/serialize_project.sh
+++ management_instance/runbooks/shared_scripts/serialize_project.sh
@@ -23,7 +23,7 @@ docker run \
   -ignoreProjectNameChanges                                                 `# allow the downstream project to change names` \
   -ignoreCacManagedValues=false                                             `# CaC enabled projects will not export the deployment process, non-secret variables, and other CaC managed project settings` \
   -ignoreProjectVariableChanges                                             `# This value is always true. Either this is an unmanaged project, in which case we are never reapplying it; or is is a variable configured project, in which case we need to ignore variable changes, or it is a shared CaC project, in which case we don't use Terraform to manage variables. ` \
-  -excludeProjectVariablesRegex "Private\..*"                               `# Exclude any variables starting with "Private."` \
+  -excludeProjectVariableRegex "Private\..*"                               `# Exclude any variables starting with "Private."` \
   -excludeRunbookRegex "__ .*"                                              `# This is a management runbook that we do not wish to export` \
   -excludeLibraryVariableSet "Octopus Server"                               `# This is library variable set used by excluded runbooks, and so we don't want to link to it in the export` \
   -excludeLibraryVariableSet "This Instance"                                `# This is library variable set used by excluded runbooks, and so we don't want to link to it in the export` \
