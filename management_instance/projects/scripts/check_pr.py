91e908795d1e3ceb6218fe05cb5ea9ccceeb4354
Matthew Casperson <matthewcasperson@gmail.com>
2023-05-14 11:32:36 +1000

    Fixing up gitea check script

--- management_instance/projects/scripts/check_pr.py
+++ management_instance/projects/scripts/check_pr.py
@@ -5,6 +5,7 @@ import shutil
 from urllib.request import Request
 import sys
 from pathlib import Path
+from urllib.parse import urlparse
 
 if "get_octopusvariable" not in globals():
     print("Script must be run as an Octopus step")
@@ -22,46 +23,66 @@ def execute(args, cwd=None):
     return stdout, stderr, retcode
 
 
-execute(['git', 'config', '--global', 'user.email', 'octopus@octopus.com'])
-execute(['git', 'config', '--global', 'user.name', 'Octopus Server'])
+try:
+    # Some dummy values expected by git.
+    execute(['git', 'config', '--global', 'user.email', 'octopus@octopus.com'])
+    execute(['git', 'config', '--global', 'user.name', 'Octopus Server'])
 
-webhook_body = get_octopusvariable("Webhook.Pr.Body") if "get_octopusvariable" in globals() else Path(
-    'test.json').read_text()
-pr = json.loads(webhook_body)
-base_repo = pr['base']['repo']['clone_url']
+    # Get the webhook body from the Octopus variable, or a test file.
+    # Comment out the check at the start of the script to use the local test file.
+    webhook_body = get_octopusvariable("Webhook.Pr.Body") if "get_octopusvariable" in globals() else Path(
+        'test.json').read_text()
+    pr = json.loads(webhook_body)
+    base_repo = pr['base']['repo']['clone_url']
 
-if os.path.exists('clone'):
-    shutil.rmtree('clone')
+    # Clean up any existing clones
+    if os.path.exists('clone'):
+        shutil.rmtree('clone')
 
-os.mkdir('clone')
+    # This is the directory the PR is cloned into
+    os.mkdir('clone')
 
-execute(['git', 'clone', base_repo.replace('localhost', 'gitea'), '.'], 'clone')
+    # Clone the base repo
+    execute(['git', 'clone', base_repo.replace('localhost', 'gitea'), '.'], 'clone')
 
-if not os.path.exists('clone/check.js') or not os.path.exists('clone/package.json'):
-    print('No check.js file in the main branch')
-    sys.exit(1)
+    # We expect to find a file called check.js (and its associated package.json file).
+    # If not, there is nothing to check
+    if not os.path.exists('clone/check.js') or not os.path.exists('clone/package.json'):
+        print('No check.js file in the main branch')
+        sys.exit(1)
 
-shutil.copy2('clone/check.js', '.')
-shutil.copy2('clone/package.json', '.')
+    # Copy the check files out of the main branch
+    shutil.copy2('clone/check.js', '.')
+    shutil.copy2('clone/package.json', '.')
 
-execute(['git', 'checkout', '-b', pr['head']['ref'], pr['base']['ref']], 'clone')
-execute(['git', 'pull', 'origin', pr['head']['ref']], 'clone')
-execute(['git', 'checkout', pr['base']['ref']], 'clone')
-execute(['git', 'merge', '--no-ff', '--no-edit', pr['head']['ref']], 'clone')
+    # Checkout the branch being merged, and initiate the merge
+    execute(['git', 'checkout', '-b', pr['head']['ref'], pr['base']['ref']], 'clone')
+    execute(['git', 'pull', 'origin', pr['head']['ref']], 'clone')
+    execute(['git', 'checkout', pr['base']['ref']], 'clone')
+    execute(['git', 'merge', '--no-ff', '--no-edit', pr['head']['ref']], 'clone')
 
-execute(['npm', 'install'])
-stdout, stderr, retcode = execute(['node', 'check.js', 'clone/.octopus/project'])
+    # Install the check file dependencies and run the check
+    execute(['npm', 'install'])
+    stdout, stderr, retcode = execute(['node', 'check.js', 'clone/.octopus/project'])
 
-print(stdout)
+    # This is the result of the check
+    print(stdout)
 
-shutil.rmtree('clone')
-shutil.rmtree('node_modules')
-os.remove('check.js')
-os.remove('package.json')
-os.remove('package-lock.json')
+    # Gitea thinks it is hosted on localhost, but we know it is hosted on "gitea"
+    parsedUrl = urlparse(pr['url'])
+    baseUrl = parsedUrl.scheme + '://gitea:' + parsedUrl.port
 
-url = "http://gitea:3000/api/v1/repos/" + pr['base']['repo']['full_name'] + "/statuses/" + pr['head']['sha']
-status = {"context": "octopus", "description": stdout, "state": "success" if retcode == 0 else "failure",
-          "target_url": "http://localhost:18080"}
+    # Post the check results back to Gitea
+    url = baseUrl + '/api/v1/repos/' + pr['base']['repo']['full_name'] + "/statuses/" + pr['head']['sha']
+    status = {"context": "octopus", "description": stdout, "state": "success" if retcode == 0 else "failure",
+              "target_url": "http://localhost:18080"}
 
-request = Request(url, headers={"Content-Type": "application/json"} or {}, data=json.dumps(status).encode("utf-8"))
+    request = Request(url, headers={"Content-Type": "application/json"} or {}, data=json.dumps(status).encode("utf-8"))
+finally:
+    # Clean everything up
+    with contextlib.suppress(FileNotFoundError):
+        shutil.rmtree('clone')
+        shutil.rmtree('node_modules')
+        os.remove('check.js')
+        os.remove('package.json')
+        os.remove('package-lock.json')
