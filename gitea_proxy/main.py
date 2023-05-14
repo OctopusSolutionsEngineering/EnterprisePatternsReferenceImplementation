c6344524dfdce7948e465d080f93d1b97a348856
Octopus Server <octopus@octopus.com>
2023-05-14 15:43:49 +1000

    Working on building Gitea webhook proxy

--- gitea_proxy/main.py
+++ gitea_proxy/main.py
@@ -1,8 +1,27 @@
 from bottle import route, run, request
+import subprocess
+
+
+def execute(args, cwd=None):
+    process = subprocess.Popen(args,
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE,
+                               text=True,
+                               cwd=cwd)
+    stdout, stderr = process.communicate()
+    retcode = process.returncode
+    return stdout, stderr, retcode
+
 
 @route('/', method='POST')
 def index(name):
     body = request.body.readlines()
-    requests.get(api_url)
+    execute(
+        ['octo', 'run-runbook', '--server', 'http://octopus:8080', '--apiKey', 'API-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA',
+         'space', 'Default',
+         "--project", 'PR Checks',
+         '--runbook', 'PR Check', '--environment', 'Sync',
+         '--variable', 'Webhook.Pr.Body:' + body])
+
 
 run(host='localhost', port=4000)
