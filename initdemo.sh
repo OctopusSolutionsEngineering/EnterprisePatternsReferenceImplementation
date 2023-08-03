37a52453145ff9c555ae31cff0edc85497969841
Matthew Casperson <matthewcasperson@gmail.com>
2023-08-03 13:13:37 +1000

    Setup an argocd user account

--- initdemo.sh
+++ initdemo.sh
@@ -136,6 +136,8 @@ then
   kubectl create namespace argocd
   kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
   kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
+  kubectl apply -f argocd/argocd-config/argocd-cm.yaml
+  kubectl apply -f argocd/argocd-config/argocd-rbac-cm.yaml
 fi
 
 # Set the initial admin Gitea user
