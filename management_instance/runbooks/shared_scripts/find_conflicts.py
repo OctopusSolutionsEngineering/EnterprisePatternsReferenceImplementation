2b2e3cba0cf1c7b538dea587603964338aef53c9
Matthew Casperson <matthewcasperson@gmail.com>
2023-09-07 07:37:17 +1000

    Updated script to support step templates

--- management_instance/runbooks/shared_scripts/find_conflicts.py
+++ management_instance/runbooks/shared_scripts/find_conflicts.py
@@ -2,6 +2,7 @@ import json
 import os
 import re
 import subprocess
+import argparse
 
 # If this script is not being run as part of an Octopus step, return variables from environment variables.
 # Periods are replaced with underscores, and the variable name is converted to uppercase
@@ -17,6 +18,18 @@ if "printverbose" not in globals():
 if "printhighlight" not in globals():
     def printhighlight(msg):
         print(msg)
+
+
+def get_octopusvariable_quiet(variable):
+    """
+    Gets an octopus variable, or an empty string if it does not exist.
+    :param variable: The variable name
+    :return: The variable value, or an empty string if the variable does not exist
+    """
+    try:
+        return get_octopusvariable(variable)
+    except:
+        return ''
 
 
 def printverbose_noansi(output):
@@ -52,33 +65,113 @@ def execute(args, cwd=None, env=None, print_args=None, print_output=printverbose
     return stdout, stderr, retcode
 
 
-cac_username = get_octopusvariable('Git.Credentials.Username')
-cac_password = get_octopusvariable('Git.Credentials.Password')
-cac_proto = get_octopusvariable('Git.Url.Protocol')
-cac_host = get_octopusvariable('Git.Url.Host')
-cac_org = get_octopusvariable('Git.Url.Organization')
-backend = re.sub('[^a-zA-Z0-9]', '_', get_octopusvariable('Octopus.Project.Name').lower())
-project_name = re.sub('[^a-zA-Z0-9]', '_', get_octopusvariable('Octopus.Project.Name').lower())
-template_repo_url = cac_proto + '://' + cac_host + '/' + cac_org + '/' + project_name + '.git'
-template_repo = cac_proto + '://' + cac_username + ':' + cac_password + '@' + cac_host + '/' + cac_org + '/' + project_name + '.git'
+def init_argparse():
+    """
+    init_argparse does triple duty. It supports running the script as a regular CLI command, supports running the
+    script in the context of a plain script step, and supports the script when embedded in a step template. This is
+    why the values of all arguments are sourced from Octopus variables, and why we source from two styles of variables:
+    regular project variables and prefixed step template variables.
+    """
+    parser = argparse.ArgumentParser(
+        usage='%(prog)s [OPTION] [FILE]...',
+        description='Find conflicts between Git repos'
+    )
+    parser.add_argument('--cac-username',
+                        action='store',
+                        default=get_octopusvariable_quiet('Git.Credentials.Username') or get_octopusvariable_quiet(
+                            'FindConflicts.Git.Credentials.Username'),
+                        help='The Git username')
+    parser.add_argument('--cac-password',
+                        action='store',
+                        default=get_octopusvariable_quiet('Git.Credentials.Password') or get_octopusvariable_quiet(
+                            'FindConflicts.Git.Credentials.Password'),
+                        help='The Git password')
+    parser.add_argument('--git-protocol',
+                        action='store',
+                        default=get_octopusvariable_quiet('Git.Url.Protocol') or get_octopusvariable_quiet(
+                            'FindConflicts.Git.Url.Protocol'),
+                        help='The Git protocol (http or https)')
+    parser.add_argument('--git-host',
+                        action='store',
+                        default=get_octopusvariable_quiet('Git.Url.Host') or get_octopusvariable_quiet(
+                            'FindConflicts.Git.Url.Host'),
+                        help='The Git hostname (e.g. github.com or gitlab.com)')
+    parser.add_argument('--git-organization',
+                        action='store',
+                        default=get_octopusvariable_quiet('Git.Url.Organization') or get_octopusvariable_quiet(
+                            'FindConflicts.Git.Url.Organization'),
+                        help='The Git organization')
+    parser.add_argument('--template-repo',
+                        action='store',
+                        default=get_octopusvariable_quiet('Git.Url.Template') or get_octopusvariable_quiet(
+                            'FindConflicts.Git.Url.Template'),
+                        help='The name of the repository holding the upstream template project')
+    parser.add_argument('--terraform-backend-type',
+                        action='store',
+                        default=get_octopusvariable_quiet('Terraform.Backend.Type') or get_octopusvariable_quiet(
+                            'FindConflicts.Terraform.Backend.Type'),
+                        help='The Terraform backend holding the state of downstream projects')
+    parser.add_argument('--terraform-backend-init-1',
+                        action='store',
+                        default=get_octopusvariable_quiet('Terraform.Backend.Init1') or get_octopusvariable_quiet(
+                            'FindConflicts.Terraform.Backend.Init1'),
+                        help='The first additional argument to pass to "terraform init", usually the "-backend-config" arguments required to connect to a custom backend')
+    parser.add_argument('--terraform-backend-init-2',
+                        action='store',
+                        default=get_octopusvariable_quiet('Terraform.Backend.Init2') or get_octopusvariable_quiet(
+                            'FindConflicts.Terraform.Backend.Init2'),
+                        help='The second additional argument to pass to "terraform init", usually the "-backend-config" arguments required to connect to a custom backend')
+    parser.add_argument('--terraform-backend-init-3',
+                        action='store',
+                        default=get_octopusvariable_quiet('Terraform.Backend.Init3') or get_octopusvariable_quiet(
+                            'FindConflicts.Terraform.Backend.Init3'),
+                        help='The third additional argument to pass to "terraform init", usually the "-backend-config" arguments required to connect to a custom backend')
+    parser.add_argument('--terraform-backend-init-4',
+                        action='store',
+                        default=get_octopusvariable_quiet('Terraform.Backend.Init4') or get_octopusvariable_quiet(
+                            'FindConflicts.Terraform.Backend.Init4'),
+                        help='The fourth additional argument to pass to "terraform init", usually the "-backend-config" arguments required to connect to a custom backend')
+    parser.add_argument('--terraform-backend-init-5',
+                        action='store',
+                        default=get_octopusvariable_quiet('Terraform.Backend.Init5') or get_octopusvariable_quiet(
+                            'FindConflicts.Terraform.Backend.Init5'),
+                        help='The fifth additional argument to pass to "terraform init", usually the "-backend-config" arguments required to connect to a custom backend')
+
+    return parser.parse_known_args()
+
+
+parser, _ = init_argparse()
+
+backend_type = parser.terraform_backend_type or 'pg'
+project_name = parser.template_repo or re.sub('[^a-zA-Z0-9]', '_', get_octopusvariable('Octopus.Project.Name').lower())
+template_repo_url = parser.git_protocol + '://' + parser.git_host + '/' + parser.git_organization + '/' + project_name + '.git'
+template_repo = parser.git_protocol + '://' + parser.cac_username + ':' + parser.cac_password + '@' + parser.git_host + '/' + parser.git_organization + '/' + project_name + '.git'
 branch = 'main'
 
 with open('backend.tf', 'w') as f:
-    f.write("""
-    terraform {
-        backend "pg" {
-      }
-      required_providers {
-        octopusdeploy = { source = "OctopusDeployLabs/octopusdeploy", version = "0.12.5" }
-      }
-    }
+    f.write(f"""
+    terraform {{
+        backend "{backend_type}" {{}}
+        required_providers {{
+          octopusdeploy = {{ source = "OctopusDeployLabs/octopusdeploy", version = "0.12.5" }}
+        }}
+    }}
     """)
 
 execute(['git', 'config', '--global', 'user.email', "octopus@octopus.com"])
 execute(['git', 'config', '--global', 'user.name', "Octopus Server"])
 
-execute(['terraform', 'init', '-no-color',
-         '-backend-config=conn_str=postgres://terraform:terraform@terraformdb:5432/' + backend + '?sslmode=disable'])
+# Allow all the init args to be supplied as variables
+custom_args = [x for x in
+               [parser.terraform_backend_init_1, parser.terraform_backend_init_2, parser.terraform_backend_init_3,
+                parser.terraform_backend_init_4, parser.terraform_backend_init_5] if x != '']
+backend = re.sub('[^a-zA-Z0-9]', '_', get_octopusvariable('Octopus.Project.Name').lower())
+# Use default values that make sense for the reference implementation if no custom values are provided
+init_args = custom_args or [
+    '-backend-config=conn_str=postgres://terraform:terraform@terraformdb:5432/' + backend + '?sslmode=disable']
+full_init_args = ['terraform', 'init', '-no-color'] + init_args
+
+execute(full_init_args)
 
 print("✓ - Up to date")
 print("▶ - Can automatically merge")
